ce10b07fc75785446320821475b5a6fad5397756
Stephanie Lakin <stephanielakin13@outlook.com>
2019-04-10 14:39:47 -0400

    adding spinner

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './spinner';
+
 
 
 class App extends React.Component {
@@ -24,9 +26,10 @@ class App extends React.Component {
     }
     // react says we have to define render!!
     render () {
-        return  <SeasonDisplay lat={this.state.lat} />
-                
-    
+       { return  <SeasonDisplay lat={this.state.lat} />,
+        
+       <Spinner message="Please accept location request..." />};
+            
     }
 }
 ReactDOM.render(<App />, document.querySelector('#root'));
